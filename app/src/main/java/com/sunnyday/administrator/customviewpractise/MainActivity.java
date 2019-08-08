e0d799794cd39476cbccbc1c56c37d6f44d713f2
sunnydaydev <1246771571@qq.com>
2019-08-08 18:04:40 +0800

    仿小米时钟 view初始化

--- app/src/main/java/com/sunnyday/administrator/customviewpractise/MainActivity.java
+++ app/src/main/java/com/sunnyday/administrator/customviewpractise/MainActivity.java
@@ -1,35 +1,41 @@
 package com.sunnyday.administrator.customviewpractise;
 
 import android.annotation.SuppressLint;
-import android.support.v4.app.Fragment;
+
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.sunnyday.administrator.customviewpractise.fragments.MiStopWatchFragment;
 import com.sunnyday.administrator.customviewpractise.fragments.WaveFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private FragmentTransaction transaction;
+
+    private FragmentManager manager;
 
     @SuppressLint("CommitTransaction")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FragmentManager manager = getSupportFragmentManager();
-        transaction = manager.beginTransaction();
+        manager = getSupportFragmentManager();
+
     }
 
-
-    private void replaceFragment(Fragment fragment) {
-        transaction.replace(R.id.fl_container,fragment);
-        transaction.commit();
-    }
 
     public void WaveLine(View view) {
-        replaceFragment(new WaveFragment());
+        FragmentTransaction transactionWave = manager.beginTransaction();
+        transactionWave.replace(R.id.fl_container, new WaveFragment());
+        transactionWave.commit();
+    }
+
+    public void XiaoMiStopWatch(View view) {
+        FragmentTransaction transactionXiaomi = manager.beginTransaction();
+        transactionXiaomi.replace(R.id.fl_container, new MiStopWatchFragment());
+        transactionXiaomi.commit();
     }
 }
---
+++ app/src/main/java/com/sunnyday/administrator/customviewpractise/customviews/MiStopWatch.java
@@ -0,0 +1,24 @@
+package com.sunnyday.administrator.customviewpractise.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by sunnyDay on 2019/8/8 17:13
+ *
+ * 小米秒表
+ */
+public class MiStopWatch extends View {
+    public MiStopWatch(Context context) {
+        super(context);
+    }
+
+    public MiStopWatch(Context context,  AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MiStopWatch(Context context,  AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
